9c5c3cd4a45aa7de57a496f65b84cdffd7d89656
Guilherme M <igusgu@hotmail.com>
2020-09-18 13:43:38 -0300

    adicionado metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -6,7 +6,7 @@
 		private $deslogin;
 		private $dessenha;
 		private $dtcadastro;
-
+//getter e setters
 		public function getIdusuario(){
 			return $this->idusuario;
 		}
@@ -34,7 +34,7 @@
 
 		public function loadById($id){//buscando no BD por um ID
 
-			$sql = new Sql();//chama a classe Sql
+			$sql = new Sql();//chama o objeto classe Sql
 
 			$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id
 			));//vai buscar no BD pelos ids e retornar um array
@@ -50,6 +50,49 @@
 			}
 
 		}
+//trazer uma lista com todos os usuarios em uma tabela//trazer uma lista com todos os usuarios em uma tabela
+		public static function getList(){
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+		}
+
+		public static function search($login){
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"
+			));
+
+		}
+		//Carrega um usuário usando login e senha
+
+		public function login($login, $password){
+
+			$sql = new Sql();//chama o objeto classe Sql
+
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password
+			));//vai buscar no BD pelos ids e retornar um array
+
+			if (count($results[0]) > 0){//validando se existe um indice 0 no BD
+
+				$row = $results[0];
+
+				$this->setIdusuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos");
+
+		}
+	}
 
 		public function __toString(){
 
--- index.php
+++ index.php
@@ -8,10 +8,29 @@
 
 	echo json_encode($usuarios);*/
 
-	$root = new Usuario();
+	//está trazendo apenas um usuário por ID específica
+	/*$root = new Usuario();
 
 	$root->loadbyId(3);
 
-	echo $root;
+	echo $root;*/
+
+	//Carrega uma lista de usuários
+/*
+	$lista = Usuario::getList();//como não há um $this no método getList foi instanciado como estático, então está buscando direto sem precisar instanciar o objeto
+
+	echo json_encode($lista);*/
+/*
+	//Carrega uma lista de usuários buscando pelo login
+	$search = Usuario::search("jo");
+
+	echo json_encode($search);*/
+
+	//Carrega um usuário usando login e senha
+
+	$usuario = new Usuario();
+	$usuario->login("root", "123");
+
+	echo $usuario;
 
 ?>
